79899bf36ba0e64bbffe6ac4e4ce541f6a02466b
henryhoangduong <henryhoangduong@gmail.com>
2025-02-25 00:47:14 +0700

    clek signin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { ConvexClientProvider } from "./ConvexClientProvider";
+import { SignIn } from "@clerk/nextjs";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ConvexClientProvider>
+          <SignIn/>
+          {children}</ConvexClientProvider>
       </body>
     </html>
   );
