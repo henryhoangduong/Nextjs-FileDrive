4698cfd5b9a62cc813effe9b3e04f9df3f7150e3
henryhoangduong <henryhoangduong@gmail.com>
2025-02-26 13:55:20 +0700

    first mutation

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -4,5 +4,5 @@ export default {
       domain: "https://witty-hawk-36.clerk.accounts.dev ",
       applicationID: "convex",
     },
-  ]
+  ],
 };
--- src/app/ConvexClientProvider.tsx
+++ src/app/ConvexClientProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {  ConvexReactClient } from "convex/react";
+import { ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
@@ -8,9 +8,13 @@ import { ConvexProviderWithClerk } from "convex/react-clerk";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-{children}
+        {children}
       </ConvexProviderWithClerk>
-    </ClerkProvider>;
+    </ClerkProvider>
+  );
 }
