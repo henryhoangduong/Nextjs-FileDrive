186e6aafea3b421ed5e3ba7830f1868e654a25f9
henryhoangduong <henryhoangduong@gmail.com>
2025-03-01 02:00:41 +0700

    fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
-import { useMutation } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 
 export default function Home() {
+  const getQueries = useQuery(api.files.getFiles);
   const creaetFile = useMutation(api.files.createFile)
   return (
     <div className="flex min-h-screen flex-col items-center justify-between  p-24">
